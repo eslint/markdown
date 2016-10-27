82d88015682de2ec2d35597bcd4b824d94a293b0
Brandon Mills <mills.brandont@gmail.com>
2016-10-26 21:52:04 -0400

    Update: Support configuration comments (fixes #46, fixes #54) (#55)

--- tests/plugin.js
+++ tests/plugin.js
@@ -21,11 +21,15 @@ describe("plugin", function() {
 
     before(function() {
         cli = new CLIEngine({
+            envs: ["browser"],
             extensions: ["md", "mkdn", "mdown", "markdown"],
             ignore: false,
             rules: {
                 "eol-last": 2,
-                "no-console": 2
+                "no-console": 2,
+                "no-undef": 2,
+                "quotes": 2,
+                "spaced-comment": 2
             },
             useEslintrc: false
         });
@@ -72,16 +76,26 @@ describe("plugin", function() {
         var report = cli.executeOnFiles([path.resolve(__dirname, "fixtures/long.md")]);
 
         assert.equal(report.results.length, 1);
-        assert.equal(report.results[0].messages.length, 4);
+        assert.equal(report.results[0].messages.length, 5);
         assert.equal(report.results[0].messages[0].message, "Unexpected console statement.");
         assert.equal(report.results[0].messages[0].line, 10);
+        assert.equal(report.results[0].messages[0].column, 1);
         assert.equal(report.results[0].messages[1].message, "Unexpected console statement.");
         assert.equal(report.results[0].messages[1].line, 16);
         assert.equal(report.results[0].messages[1].column, 5);
         assert.equal(report.results[0].messages[2].message, "Unexpected console statement.");
-        assert.equal(report.results[0].messages[2].line, 21);
-        assert.equal(report.results[0].messages[3].message, "Unexpected console statement.");
-        assert.equal(report.results[0].messages[3].line, 27);
+        assert.equal(report.results[0].messages[2].line, 24);
+        assert.equal(report.results[0].messages[2].column, 1);
+        assert.equal(report.results[0].messages[3].message, "Strings must use singlequote.");
+        assert.equal(report.results[0].messages[3].line, 38);
+        assert.equal(report.results[0].messages[3].column, 13);
+        assert.equal(report.results[0].messages[4].message, "Parsing error: Unexpected character '@'");
+        assert.equal(report.results[0].messages[4].line, 46);
+        assert.equal(report.results[0].messages[4].column, 2);
+    });
+
+    describe("disable comments", function() {
+
     });
 
 });
