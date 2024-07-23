19a2d551f50a64c7f5206061b39c1c8bff25a1e8
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-07-23 16:06:35 -0400

    chore: Add type checking

---
+++ tools/dedupe-types.js
@@ -0,0 +1,43 @@
+/**
+ * @fileoverview Strips typedef aliases from the rolled-up file. This
+ * is necessary because the TypeScript compiler throws an error when
+ * it encounters a duplicate typedef.
+ *
+ * Usage:
+ *  node scripts/strip-typedefs.js filename1.js filename2.js ...
+ *
+ * @author Nicholas C. Zakas
+ */
+
+//-----------------------------------------------------------------------------
+// Imports
+//-----------------------------------------------------------------------------
+
+import fs from "node:fs";
+
+//-----------------------------------------------------------------------------
+// Main
+//-----------------------------------------------------------------------------
+
+// read files from the command line
+const files = process.argv.slice(2);
+
+files.forEach(filePath => {
+	const lines = fs.readFileSync(filePath, "utf8").split(/\r?\n/gu);
+	const typedefs = new Set();
+
+	const remainingLines = lines.filter(line => {
+		if (!line.startsWith("/** @typedef {import")) {
+			return true;
+		}
+
+		if (typedefs.has(line)) {
+			return false;
+		}
+
+		typedefs.add(line);
+		return true;
+	});
+
+	fs.writeFileSync(filePath, remainingLines.join("\n"), "utf8");
+});
