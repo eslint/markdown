5dd8e74d6c3eff6009b2d2bceb4ce3f635709b69
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-07-23 16:56:16 -0400

    feat: Add type checking

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+import type { Node } from "mdast";
+import type { Linter } from "eslint";
+
+
+export interface RangeMap {
+    indent: number;
+    js: number;
+    md: number;
+}
+
+export interface BlockBase {
+    baseIndentText: string;
+    comments: string[];
+    rangeMap: RangeMap[];
+}
+
+export interface Block extends Node, BlockBase {}
+
+export type Message = Linter.LintMessage;
