0503748497e623842b8c78b140b766a086ee2ce7
Nicholas C. Zakas <nicholas@nczconsulting.com>
2024-08-05 10:54:01 -0400

    feat: Add type checking and type definitions (#266)
    * chore: Add type checking
    
    * feat: Add type checking
    
    * Fix lint errors

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import copy from "rollup-plugin-copy";
+
+export default {
+    input: "src/index.js",
+    output: [
+        {
+            file: "dist/esm/index.js",
+            format: "esm",
+            banner: '// @ts-self-types="./index.d.ts"'
+        }
+    ],
+    plugins: [
+        copy({
+            targets: [
+                { src: "src/types.ts", dest: "dist/esm" }
+            ]
+        })
+    ]
+};
