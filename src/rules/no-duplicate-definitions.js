c365bf29f29cbaafcc923593062d0656d6ab3238
루밀LuMir <rpfos@naver.com>
2025-05-07 18:21:12 +0900

    wip: add more testcases

--- src/rules/no-duplicate-definitions.js
+++ src/rules/no-duplicate-definitions.js
@@ -49,7 +49,7 @@ export default {
 
 		defaultOptions: [
 			{
-				ignores: ["//", "comment"],
+				ignores: ["//"],
 			},
 		],
 	},
---
+++ tests/rules/no-duplicate-definitions.test.js
@@ -0,0 +1,90 @@
+/**
+ * @fileoverview Tests for no-duplicate-definitions rule.
+ * @author 루밀LuMir(lumirlumir)
+ */
+
+//------------------------------------------------------------------------------
+// Imports
+//------------------------------------------------------------------------------
+
+import rule from "../../src/rules/no-duplicate-definitions.js";
+import markdown from "../../src/index.js";
+import { RuleTester } from "eslint";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+	plugins: {
+		markdown,
+	},
+	language: "markdown/commonmark",
+});
+
+ruleTester.run("no-duplicate-definitions", rule, {
+	valid: [
+		`
+[mercury]: https://example.com/mercury/
+`,
+		`
+[mercury]: https://example.com/mercury/
+[venus]: https://example.com/venus/
+`,
+	],
+	invalid: [
+		{
+			code: `
+[mercury]: https://example.com/mercury/
+[mercury]: https://example.com/venus/
+`,
+			errors: [
+				{
+					messageId: "duplicateDefinition",
+					line: 2,
+					column: 1,
+					endLine: 2,
+					endColumn: 40,
+				},
+				{
+					messageId: "duplicateDefinition",
+					line: 3,
+					column: 1,
+					endLine: 3,
+					endColumn: 38,
+				},
+			],
+		},
+		{
+			code: `
+[mercury]: https://example.com/mercury/
+[earth]: https://example.com/earth/
+[mars]: https://example.com/mars/
+
+[//]: # (comment about mars)
+
+[jupiter]: https://example.com/jupiter/
+
+[//]: # (comment about jupiter)
+
+[mercury]: https://example.com/venus/
+`,
+			errors: [
+				{
+					messageId: "duplicateDefinition",
+					line: 2,
+					column: 1,
+					endLine: 2,
+					endColumn: 40,
+				},
+				{
+					messageId: "duplicateDefinition",
+					line: 12,
+					column: 1,
+					endLine: 12,
+					endColumn: 38,
+				},
+			],
+		},
+	],
+});
