21f0b5c38fedd6491c50847f0cdfc4049f3da37c
xbinaryx <xbinaryx68@gmail.com>
2025-05-29 15:14:57 +0300

    feat: add siblingsOnly option to no-duplicate-headings rule

--- src/rules/no-duplicate-headings.js
+++ src/rules/no-duplicate-headings.js
@@ -7,8 +7,9 @@
 // Type Definitions
 //-----------------------------------------------------------------------------
 
+/** @typedef {import("mdast").Heading} HeadingNode */
 /**
- * @typedef {import("../types.ts").MarkdownRuleDefinition<{ RuleOptions: []; }>}
+ * @typedef {import("../types.ts").MarkdownRuleDefinition<{ RuleOptions: [{ siblingsOnly?: boolean; }]; }>}
  * NoDuplicateHeadingsRuleDefinition
  */
 
@@ -29,46 +30,88 @@ export default {
 		messages: {
 			duplicateHeading: 'Duplicate heading "{{text}}" found.',
 		},
+
+		schema: [
+			{
+				type: "object",
+				properties: {
+					siblingsOnly: {
+						type: "boolean",
+					},
+				},
+				additionalProperties: false,
+			},
+		],
+
+		defaultOptions: [{ siblingsOnly: false }],
 	},
 
 	create(context) {
-		const headings = new Set();
+		const [{ siblingsOnly }] = context.options;
 		const { sourceCode } = context;
+
+		const headingsByLevel = [null, []];
+		let lastLevel = 1;
+		let currentLevelHeadings = headingsByLevel[lastLevel];
+
+		/**
+		 * Gets the text of a heading node
+		 * @param {HeadingNode} node The heading node
+		 * @returns {string} The heading text
+		 */
+		function getHeadingText(node) {
+			/*
+			 * There are two types of headings in markdown:
+			 * - ATX headings, which start with one or more # characters
+			 * - Setext headings, which are underlined with = or -
+			 * Setext headings are identified by being on two lines instead of one,
+			 * with the second line containing only = or - characters. In order to
+			 * get the correct heading text, we need to determine which type of
+			 * heading we're dealing with.
+			 */
+			const isSetext =
+				node.position.start.line !== node.position.end.line;
+
+			return isSetext
+				? // get only the text from the first line
+					sourceCode.lines[node.position.start.line - 1].trim()
+				: // get the text without the leading # characters
+					sourceCode
+						.getText(node)
+						.slice(node.depth + 1)
+						.trim();
+		}
 
 		return {
 			heading(node) {
-				/*
-				 * There are two types of headings in markdown:
-				 * - ATX headings, which start with one or more # characters
-				 * - Setext headings, which are underlined with = or -
-				 * Setext headings are identified by being on two lines instead of one,
-				 * with the second line containing only = or - characters. In order to
-				 * get the correct heading text, we need to determine which type of
-				 * heading we're dealing with.
-				 */
-				const isSetext =
-					node.position.start.line !== node.position.end.line;
+				const headingText = getHeadingText(node);
 
-				const text = isSetext
-					? // get only the text from the first line
-						sourceCode.lines[node.position.start.line - 1].trim()
-					: // get the text without the leading # characters
-						sourceCode
-							.getText(node)
-							.slice(node.depth + 1)
-							.trim();
+				if (siblingsOnly) {
+					const currentLevel = node.depth;
+					while (lastLevel < currentLevel) {
+						lastLevel++;
+						headingsByLevel[lastLevel] = [];
+					}
 
-				if (headings.has(text)) {
+					while (lastLevel > currentLevel) {
+						headingsByLevel[lastLevel] = [];
+						lastLevel--;
+					}
+
+					currentLevelHeadings = headingsByLevel[currentLevel];
+				}
+
+				if (currentLevelHeadings.includes(headingText)) {
 					context.report({
 						loc: node.position,
 						messageId: "duplicateHeading",
 						data: {
-							text,
+							text: headingText,
 						},
 					});
+				} else {
+					currentLevelHeadings.push(headingText);
 				}
-
-				headings.add(text);
 			},
 		};
 	},
--- tests/rules/no-duplicate-headings.test.js
+++ tests/rules/no-duplicate-headings.test.js
@@ -10,6 +10,7 @@
 import rule from "../../src/rules/no-duplicate-headings.js";
 import markdown from "../../src/index.js";
 import { RuleTester } from "eslint";
+import dedent from "dedent";
 
 //------------------------------------------------------------------------------
 // Tests
@@ -27,6 +28,51 @@ ruleTester.run("no-duplicate-headings", rule, {
 		`# Heading 1
 
         ## Heading 2`,
+		{
+			code: dedent`
+				# Change log
+
+				## 1.0.0
+
+				### Features
+
+				## 2.0.0
+
+				### Features
+			`,
+			options: [{ siblingsOnly: true }],
+		},
+		{
+			code: dedent`
+				1.0.0
+				=====
+
+				Features
+				--------
+
+				2.0.0
+				=====
+
+				Features
+				--------
+			`,
+			options: [{ siblingsOnly: true }],
+		},
+		{
+			code: dedent`
+				1.0.0
+				=====
+
+				Features
+				--------
+
+				2.0.0
+				=====
+
+				## Features
+			`,
+			options: [{ siblingsOnly: true }],
+		},
 	],
 	invalid: [
 		{
@@ -92,6 +138,156 @@ Heading 1
 					column: 1,
 					endLine: 5,
 					endColumn: 10,
+				},
+			],
+		},
+		{
+			code: dedent`
+				# Heading 1
+
+				# Heading 1
+            `,
+			options: [{ siblingsOnly: true }],
+			errors: [
+				{
+					messageId: "duplicateHeading",
+					line: 3,
+					column: 1,
+					endLine: 3,
+					endColumn: 12,
+				},
+			],
+		},
+		{
+			code: dedent`
+				# Section 1
+
+				## Subsection A
+				## Subsection A
+
+				# Section 2
+
+				## Subsection B
+			`,
+			options: [{ siblingsOnly: true }],
+			errors: [
+				{
+					messageId: "duplicateHeading",
+					line: 4,
+					column: 1,
+					endLine: 4,
+					endColumn: 16,
+				},
+			],
+		},
+		{
+			code: dedent`
+				# Section 1
+
+				## Subsection A
+				## Subsection B
+
+				# Section 2
+
+				## Subsection A
+				## Subsection A
+			`,
+			options: [{ siblingsOnly: true }],
+			errors: [
+				{
+					messageId: "duplicateHeading",
+					line: 9,
+					column: 1,
+					endLine: 9,
+					endColumn: 16,
+				},
+			],
+		},
+		{
+			code: dedent`
+				Section 1
+				=========
+
+				Subsection A
+				------------
+				Subsection A
+				------------
+
+				Section 2
+				=========
+
+				Subsection B
+				------------
+			`,
+			options: [{ siblingsOnly: true }],
+			errors: [
+				{
+					messageId: "duplicateHeading",
+					line: 6,
+					column: 1,
+					endLine: 7,
+					endColumn: 13,
+				},
+			],
+		},
+		{
+			code: dedent`
+				Section 1
+				=========
+
+				Subsection A
+				------------
+				Subsection B
+				------------
+
+				Section 2
+				=========
+
+				Subsection A
+				------------
+				Subsection A
+				------------
+			`,
+			options: [{ siblingsOnly: true }],
+			errors: [
+				{
+					messageId: "duplicateHeading",
+					line: 14,
+					column: 1,
+					endLine: 15,
+					endColumn: 13,
+				},
+			],
+		},
+		{
+			code: dedent`
+				# Section 1
+
+				## Subsection A
+				## Subsection B
+
+				# Section 2
+
+				## Subsection A
+				## Subsection A
+				Subsection A
+				------------
+			`,
+			options: [{ siblingsOnly: true }],
+			errors: [
+				{
+					messageId: "duplicateHeading",
+					line: 9,
+					column: 1,
+					endLine: 9,
+					endColumn: 16,
+				},
+				{
+					messageId: "duplicateHeading",
+					line: 10,
+					column: 1,
+					endLine: 11,
+					endColumn: 13,
 				},
 			],
 		},
