8ff94c261bc3ce7750eaf9d375c2d618359a215e
루밀LuMir <rpfos@naver.com>
2025-07-28 23:50:26 +0900

    wip: remove `markdown-it`

--- src/html/test/markdown.test.ts
+++ src/html/test/markdown.test.ts
@@ -1,20 +1,10 @@
 import { parse, renderSync } from "../src/";
 import { describe, expect, it } from "vitest";
-import Markdown from "markdown-it";
-
-const md = new Markdown();
-
-const src = `Token CSS is a new tool that seamlessly integrates [Design Tokens](https://design-tokens.github.io/community-group/format/#design-token) into your development workflow. Conceptually, it is similar to tools
-like [Tailwind](https://tailwindcss.com), [Styled System](https://styled-system.com/), and many CSS-in-JS libraries that provide tokenized _constraints_ for your styles&mdash;but there's one big difference.
-
-# Hello world!
-
-**Token CSS embraces \`.css\` files and \`<style>\` blocks.**
-`;
 
 describe("markdown", () => {
 	it("works", async () => {
-		const input = md.render(src);
+		const input =
+			'<p>Token CSS is a new tool that seamlessly integrates <a href="https://design-tokens.github.io/community-group/format/#design-token">Design Tokens</a> into your development workflow. Conceptually, it is similar to tools <a href="https://tailwindcss.com">Tailwind</a>, <a href="https://styled-system.com/">Styled System</a>, and many CSS-in-JS libraries that provide tokenized <em>constraints</em> for your styles—but there\'s one big difference.</p>\t<h1>Hello world!</h1><p><strong>Token CSS embraces <code>.css</code> files and <code>&lt;style&gt;</code> blocks.</strong></p>';
 		const output = renderSync(parse(input));
 		expect(input).eq(output);
 	});
