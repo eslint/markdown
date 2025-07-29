31172b2400f59965a21fc5a5a64329ceacc96e9e
루밀LuMir <rpfos@naver.com>
2025-07-30 01:20:11 +0900

    wip: cleanup tests

--- src/html/basic.test.ts
+++ src/html/basic.test.ts
@@ -1,3 +1,62 @@
+/**
+ * @fileoverview Tests for the html.js
+ * @author Json Miller, Nate Moore, 루밀LuMir(lumirlumir)
+ * @license MIT
+ *
+ * ---
+ *
+ * Portions of this code were borrowed from `ultrahtml` - https://github.com/natemoo-re/ultrahtml
+ *
+ * MIT License Copyright (c) 2022 Nate Moore
+ *
+ * Permission is hereby granted, free of
+ * charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ * The above copyright notice and this permission notice
+ * (including the next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * ---
+ *
+ * Portions of this code were borrowed from `htmlParser` - https://github.com/developit/htmlParser
+ *
+ * The MIT License (MIT) Copyright (c) 2013 Jason Miller
+ *
+ * Permission is hereby granted, free of
+ * charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+//-----------------------------------------------------------------------------
+// Imports
+//-----------------------------------------------------------------------------
+
 import assert from "node:assert";
 import {
 	parse,
@@ -9,6 +68,10 @@ import {
 } from "./index.js";
 import { describe, expect, it } from "vitest";
 
+//-----------------------------------------------------------------------------
+// Tests
+//-----------------------------------------------------------------------------
+
 describe("html", () => {
 	it("sanity", () => {
 		assert.strictEqual(typeof parse, "function");
@@ -18,7 +81,7 @@ describe("html", () => {
 		assert.strictEqual(typeof querySelectorAll, "function");
 	});
 
-	describe("basic", () => {
+	describe("parse(), renderSync()", () => {
 		describe("input === output", () => {
 			it("works for elements", async () => {
 				const input = `<h1>Hello world!</h1>`;
@@ -50,6 +113,12 @@ describe("html", () => {
 				const output = renderSync(parse(input));
 
 				expect(output).toEqual(input);
+			});
+			it("works for long inputs", async () => {
+				const input =
+					'<p>Token CSS is a new tool that seamlessly integrates <a href="https://design-tokens.github.io/community-group/format/#design-token">Design Tokens</a> into your development workflow. Conceptually, it is similar to tools <a href="https://tailwindcss.com">Tailwind</a>, <a href="https://styled-system.com/">Styled System</a>, and many CSS-in-JS libraries that provide tokenized <em>constraints</em> for your styles—but there\'s one big difference.</p>\t<h1>Hello world!</h1><p><strong>Token CSS embraces <code>.css</code> files and <code>&lt;style&gt;</code> blocks.</strong></p>';
+				const output = renderSync(parse(input));
+				expect(input).eq(output);
 			});
 		});
 
@@ -115,73 +184,72 @@ more&quot;"></div>`);
 				});
 			});
 		});
-	});
 
-	describe("markdown", () => {
-		it("works", async () => {
-			const input =
-				'<p>Token CSS is a new tool that seamlessly integrates <a href="https://design-tokens.github.io/community-group/format/#design-token">Design Tokens</a> into your development workflow. Conceptually, it is similar to tools <a href="https://tailwindcss.com">Tailwind</a>, <a href="https://styled-system.com/">Styled System</a>, and many CSS-in-JS libraries that provide tokenized <em>constraints</em> for your styles—but there\'s one big difference.</p>\t<h1>Hello world!</h1><p><strong>Token CSS embraces <code>.css</code> files and <code>&lt;style&gt;</code> blocks.</strong></p>';
-			const output = renderSync(parse(input));
-			expect(input).eq(output);
-		});
-	});
-
-	describe("script", () => {
-		it("works for elements", async () => {
-			const input = `<script>console.log("Hello <name>!")</script>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
-		});
-		it("works without quotes", async () => {
-			const input = `<script>0<1>0</name></script>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
-		});
-		it("works with <script> in string", async () => {
-			const input = `<head><meta charset="utf-8"/><meta name="viewport" content="width=device-width,initial-scale=1"/><link rel="icon" href="/favicon-48x48.cbbd161b.png"/><link rel="apple-touch-icon" href="/apple-touch-icon.6803c6f0.png"/><meta name="theme-color" content="#ffffff"/><link rel="manifest" href="/manifest.56b1cedc.json"/><link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="MDN Web Docs"/><script>Array.prototype.flat&&Array.prototype.includes||document.write('<script src="https://polyfill.io/v3/polyfill.min.js?features=Array.prototype.flat%2Ces6"><\/script>')</script><title>HTML Sanitizer API - Web APIs | MDN</title><link rel="alternate" title="HTML Sanitizer API" href="https://developer.mozilla.org/ja/docs/Web/API/HTML_Sanitizer_API" hreflang="ja"/><link rel="alternate" title="HTML Sanitizer API" href="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API" hreflang="en"/><meta name="robots" content="index, follow"><meta name="description" content="The HTML Sanitizer API allow developers to take untrusted strings of HTML and Document or DocumentFragment objects, and sanitize them for safe insertion into a document&apos;s DOM."/><meta property="og:url" content="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API"/><meta property="og:title" content="HTML Sanitizer API - Web APIs | MDN"/><meta property="og:locale" content="en-US"/><meta property="og:description" content="The HTML Sanitizer API allow developers to take untrusted strings of HTML and Document or DocumentFragment objects, and sanitize them for safe insertion into a document&apos;s DOM."/><meta property="og:image" content="https://developer.mozilla.org/mdn-social-share.cd6c4a5a.png"/><meta property="twitter:card" content="summary_large_image"/><link rel="canonical" href="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API"/><style media="print">.breadcrumbs-container,.document-toc-container,.language-menu,.on-github,.page-footer,.top-navigation-main,nav.sidebar,ul.prev-next{display:none!important}.main-page-content,.main-page-content pre{padding:2px}.main-page-content pre{border-left-width:2px}</style><script src="/static/js/ga.js" defer=""></script><script defer="defer" src="/static/js/main.bfba1cdc.js"></script><link href="/static/css/main.7fcd0907.css" rel="stylesheet">`;
-			let meta = 0;
-			walkSync(parse(input), async (node, parent) => {
-				if (
-					node.type === ELEMENT_NODE &&
-					node.name === "meta" &&
-					parent?.name === "head"
-				) {
-					meta++;
-				}
+		describe("script", () => {
+			it("works for elements", async () => {
+				const input = `<script>console.log("Hello <name>!")</script>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
 			});
-			expect(meta).toEqual(11);
+			it("works without quotes", async () => {
+				const input = `<script>0<1>0</name></script>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
+			});
+			it("works with <script> in string", async () => {
+				const input = `<head><meta charset="utf-8"/><meta name="viewport" content="width=device-width,initial-scale=1"/><link rel="icon" href="/favicon-48x48.cbbd161b.png"/><link rel="apple-touch-icon" href="/apple-touch-icon.6803c6f0.png"/><meta name="theme-color" content="#ffffff"/><link rel="manifest" href="/manifest.56b1cedc.json"/><link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="MDN Web Docs"/><script>Array.prototype.flat&&Array.prototype.includes||document.write('<script src="https://polyfill.io/v3/polyfill.min.js?features=Array.prototype.flat%2Ces6"><\/script>')</script><title>HTML Sanitizer API - Web APIs | MDN</title><link rel="alternate" title="HTML Sanitizer API" href="https://developer.mozilla.org/ja/docs/Web/API/HTML_Sanitizer_API" hreflang="ja"/><link rel="alternate" title="HTML Sanitizer API" href="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API" hreflang="en"/><meta name="robots" content="index, follow"><meta name="description" content="The HTML Sanitizer API allow developers to take untrusted strings of HTML and Document or DocumentFragment objects, and sanitize them for safe insertion into a document&apos;s DOM."/><meta property="og:url" content="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API"/><meta property="og:title" content="HTML Sanitizer API - Web APIs | MDN"/><meta property="og:locale" content="en-US"/><meta property="og:description" content="The HTML Sanitizer API allow developers to take untrusted strings of HTML and Document or DocumentFragment objects, and sanitize them for safe insertion into a document&apos;s DOM."/><meta property="og:image" content="https://developer.mozilla.org/mdn-social-share.cd6c4a5a.png"/><meta property="twitter:card" content="summary_large_image"/><link rel="canonical" href="https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API"/><style media="print">.breadcrumbs-container,.document-toc-container,.language-menu,.on-github,.page-footer,.top-navigation-main,nav.sidebar,ul.prev-next{display:none!important}.main-page-content,.main-page-content pre{padding:2px}.main-page-content pre{border-left-width:2px}</style><script src="/static/js/ga.js" defer=""></script><script defer="defer" src="/static/js/main.bfba1cdc.js"></script><link href="/static/css/main.7fcd0907.css" rel="stylesheet">`;
+				let meta = 0;
+				walkSync(parse(input), async (node, parent) => {
+					if (
+						node.type === ELEMENT_NODE &&
+						node.name === "meta" &&
+						parent?.name === "head"
+					) {
+						meta++;
+					}
+				});
+				expect(meta).toEqual(11);
+			});
+			it("works with <script> inside script", async () => {
+				const input = `<script>const a = "<script>"</script>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
+			});
+			it("works with <any> inside script", async () => {
+				const input = `<script>const a = "<any>"</script>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
+			});
+			it("works with <\\/script> inside script", async () => {
+				const input = `<script>const a = "<\\/script>"</script>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
+			});
 		});
-		it("works with <script> inside script", async () => {
-			const input = `<script>const a = "<script>"</script>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
+
+		describe("style", () => {
+			it("works for elements", async () => {
+				const input = `<style><name></name><foo></style>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
+			});
+			it("works without quotes", async () => {
+				const input = `<style>0>1</name></style>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
+			});
 		});
-		it("works with <any> inside script", async () => {
-			const input = `<script>const a = "<any>"</script>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
-		});
-		it("works with <\\/script> inside script", async () => {
-			const input = `<script>const a = "<\\/script>"</script>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
+
+		describe("svg", () => {
+			it("renderSync as self-closing", async () => {
+				const input = `<svg><path d="0 0 0" /></svg>`;
+				const output = renderSync(parse(input));
+				expect(output).toEqual(input);
+			});
 		});
 	});
 
-	describe("style", () => {
-		it("works for elements", async () => {
-			const input = `<style><name></name><foo></style>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
-		});
-		it("works without quotes", async () => {
-			const input = `<style>0>1</name></style>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
-		});
-	});
-
-	describe("selector", () => {
+	describe("querySelector()", () => {
 		describe("type selector", () => {
 			it("type", async () => {
 				const input = `<h1>Hello world!</h1>`;
@@ -201,13 +269,5 @@ more&quot;"></div>`);
 		});
 
 		*/
-	});
-
-	describe("svg", () => {
-		it("renderSync as self-closing", async () => {
-			const input = `<svg><path d="0 0 0" /></svg>`;
-			const output = renderSync(parse(input));
-			expect(output).toEqual(input);
-		});
 	});
 });
