a64a7dcfbc73b6621f8adcfced96f0158065473c
루밀LuMir <rpfos@naver.com>
2025-07-30 16:50:59 +0900

    wip: add more tests

--- tests/html.test.js
+++ tests/html.test.js
@@ -81,45 +81,59 @@ describe("html", () => {
 		describe("input === output", () => {
 			it("works for elements", async () => {
 				const input = `<h1>Hello world!</h1>`;
-				const output = renderSync(parse(input));
+				const ast = parse(input);
+				const output = renderSync(ast);
 
+				assert.strictEqual(ast.type, "document");
 				assert.strictEqual(output, input);
 			});
 			it("works for custom elements", async () => {
 				const input = `<custom-element>Hello world!</custom-element>`;
-				const output = renderSync(parse(input));
+				const ast = parse(input);
+				const output = renderSync(ast);
 
+				assert.strictEqual(ast.type, "document");
 				assert.strictEqual(output, input);
 			});
 			it("works for comments", async () => {
 				const input = `<!--foobar-->`;
-				const output = renderSync(parse(input));
+				const ast = parse(input);
+				const output = renderSync(ast);
 
+				assert.strictEqual(ast.type, "document");
 				assert.strictEqual(output, input);
 			});
 			it("works for text", async () => {
 				const input = `Hmm...`;
-				const output = renderSync(parse(input));
+				const ast = parse(input);
+				const output = renderSync(ast);
 
+				assert.strictEqual(ast.type, "document");
 				assert.strictEqual(output, input);
 			});
 			it("works for doctype", async () => {
 				const input = `<!DOCTYPE html>`;
-				const output = renderSync(parse(input));
+				const ast = parse(input);
+				const output = renderSync(ast);
 
+				assert.strictEqual(ast.type, "document");
 				assert.strictEqual(output, input);
 			});
 			it("works for html:5", async () => {
 				const input = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body></body></html>`;
-				const output = renderSync(parse(input));
+				const ast = parse(input);
+				const output = renderSync(ast);
 
+				assert.strictEqual(ast.type, "document");
 				assert.strictEqual(output, input);
 			});
 			it("works for long inputs", async () => {
 				const input =
 					'<p>Token CSS is a new tool that seamlessly integrates <a href="https://design-tokens.github.io/community-group/format/#design-token">Design Tokens</a> into your development workflow. Conceptually, it is similar to tools <a href="https://tailwindcss.com">Tailwind</a>, <a href="https://styled-system.com/">Styled System</a>, and many CSS-in-JS libraries that provide tokenized <em>constraints</em> for your styles—but there\'s one big difference.</p>\t<h1>Hello world!</h1><p><strong>Token CSS embraces <code>.css</code> files and <code>&lt;style&gt;</code> blocks.</strong></p>';
-				const output = renderSync(parse(input));
+				const ast = parse(input);
+				const output = renderSync(ast);
 
+				assert.strictEqual(ast.type, "document");
 				assert.strictEqual(output, input);
 			});
 		});
