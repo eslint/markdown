5759d192556470e941a7f4d62b0257b00ee92020
루밀LuMir <rpfos@naver.com>
2025-10-07 17:04:42 +0900

    wip: remove `findOffsets` util

--- src/util.js
+++ src/util.js
@@ -24,34 +24,6 @@ export const htmlCommentPattern = /<!--[\s\S]*?-->/gu;
 //-----------------------------------------------------------------------------
 
 /**
- * Finds the line and column offsets for a given offset in a string.
- * @param {string} text The text to search.
- * @param {number} offset The offset to find.
- * @returns {{lineOffset:number,columnOffset:number}} The location of the offset.
- *      Note that `columnOffset` should be used as an offset to the column number
- *      of the given text in the source code only when `lineOffset` is 0.
- *      Otherwise, it should be used as a 0-based column number in the source code.
- */ // TODO
-export function findOffsets(text, offset) {
-	let lineOffset = 0;
-	let columnOffset = 0;
-
-	for (let i = 0; i < offset; i++) {
-		if (text[i] === "\n") {
-			lineOffset++;
-			columnOffset = 0;
-		} else {
-			columnOffset++;
-		}
-	}
-
-	return {
-		lineOffset,
-		columnOffset,
-	};
-}
-
-/**
  * Checks if a frontmatter block contains a title matching the given pattern
  * @param {string} value The frontmatter content
  * @param {RegExp|null} pattern The pattern to match against
