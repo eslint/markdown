dc4267d09251d36208c7c1474d8f3d00dc6c8704
루밀LuMir <rpfos@naver.com>
2025-11-09 21:32:05 +0900

    chore: correct regex pattern for `eslint-plugin/report-message-format` (#581)

--- eslint.config.js
+++ eslint.config.js
@@ -98,7 +98,7 @@ export default defineConfig([
 			"eslint-plugin/require-meta-schema": "off", // `schema` defaults to []
 			"eslint-plugin/prefer-placeholders": "error",
 			"eslint-plugin/prefer-replace-text": "error",
-			"eslint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
+			"eslint-plugin/report-message-format": ["error", "^[^a-z].*\\.$"],
 			"eslint-plugin/require-meta-docs-description": [
 				"error",
 				{ pattern: "^(Enforce|Require|Disallow) .+[^. ]$" },
